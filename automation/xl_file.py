bafff72297b8250788fb216ac1145f854a53f3dd
shivashankar101 <shivashankarawati@gmail.com>
2021-01-27 16:03:53 +0530

    new concepts

---
+++ automation/xl_file.py
@@ -0,0 +1,29 @@
+import openpyxl
+from openpyxl import Workbook
+
+#workbook = Workbook()
+path = "/Users/shiv/desktop/3.xlsx"
+workbook = openpyxl.load_workbook(path)
+
+sheet1 = workbook.active
+#sheet2 = workbook.get_sheet_by_name("sheet2")
+
+rows = sheet1.max_row
+cols = sheet1.max_column
+
+print(rows)
+print(cols)
+
+for r in range(4, 8):
+    for c in range(1, cols+1):
+        sheet1.cell(row=r, column=c).value = "its okay"
+
+workbook.save(path)
+
+
+for r in range(1, rows+1):
+    for c in range(1, cols+1):
+        print(sheet1.cell(row=r, column=c).value, end="     ")
+
+    print()
+
