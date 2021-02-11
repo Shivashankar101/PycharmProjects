8eac1a741e8e424bd962d18fa4b104b93f10e6c5
shivashankar101 <shivashankarawati@gmail.com>
2021-02-11 12:37:53 +0530

    hi

--- automation/Pacage1/TC_login2.py
+++ automation/Pacage1/TC_login2.py
@@ -2,113 +2,58 @@ import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import XLUtils as xl
 
 
 driver = webdriver.Firefox(executable_path="/Users/shiv/Desktop/drivers/gecko/geckodriver")
 act = ActionChains(driver)
 
 
-class LoginTest(unittest.TestCase):
+xl_sheet_path = '/Users/shiv/Desktop/logindata.xlsx'
+url = xl.read_data(xl_sheet_path, 'sheet1', row=1, col=2)
+rows = xl.count_rows(xl_sheet_path, 'sheet1')
+cols = xl.count_clos(xl_sheet_path, 'sheet1')
 
+
+class LoginTest(unittest.TestCase):
+    
     @classmethod
     def setUpClass(cls):
         cls.driver = driver
 
-    # def test_homepage(self):
-    #     driver.get(url)
-    #     self.assertEqual(page_title, driver.title , "page title is not matching")
-    #
-    # def test_log_in(self):
-    #     login_tab.send_keys(login_id).submit()
-    #     password_tab.send_keys(password).submit()
-
-
-
-    @unittest.SkipTest
-    def test_by_email(self):
-        try:
-            url = 'https://stackoverflow.com/'
-            driver.get(url)
-
-            if driver.title == " Ask Question ":
-                driver.get("https://mail.google.com")
-                driver.save_screenshot('/Users/shiv/PycharmProjects/automation/screenshots/gmail3.png')
-
-            else:
-                login = WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, "/html/body/header/div/ol[2]/li[2]/a[1]")))
-                login.click()
-                with_google = driver.find_element_by_xpath("//*[@id='openid-buttons']/button[1]")
-                with_google.click()
-                acc1 = driver.find_element_by_xpath("//*[@id='view_container']/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/div/ul/li[1]/div/div[1]/div")
-                another_acc = driver.find_element_by_xpath(
-                    "//*[@id='view_container']/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/div/ul/li[2]/div/div/div[2]")
-                login = driver.find_element_by_name("identifier")
-                password = driver.find_element_by_name("password")
-
-                if acc1.is_displayed():
-                    acc1.click()
-                elif another_acc.is_displayed():
-                    another_acc.click()
-                    login.send_keys("vishwamanava5").submit()
-                    password.send_keys("Pavan@keshav").submit()
-                else:
-                    login.send_keys("vishwamanava5").submit()
-                    password.send_keys("Pavan@keshav").submit()
-                # login.click().send_keys(id)
-
-                # user.submit()
-                # time.sleep(3)
-                # pw = driver.find_element(By.NAME, "password")
-                # pw.send_keys(password)
-                # pw.send_keys(Keys.ENTER)
-                driver.get("https://mail.google.com")
-                time.sleep(3)
-                driver.save_screenshot('/Users/shiv/PycharmProjects/automation/screenshots/gmail4.png')
-        except AttributeError:
-            driver.save_screenshot('/Users/shiv/PycharmProjects/automation/screenshots/gmail_error2.png')
-
-    # @unittest.SkipTest
-    def test_by_fb(self):
-        url = 'https://www.facebook.com/'
-        id = 'shivashankarawati@gmail.com'
-        password = 'Pgpgpgagj100.'
+    def setUp(self):
         driver.get(url)
-        user = WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.NAME, "email")))
-        user.send_keys(id)
-        user.send_keys(Keys.ENTER)
-        time.sleep(3)
-        pw = driver.find_element(By.NAME, "pass")
-        pw.send_keys(password)
-        pw.send_keys(Keys.ENTER)
+        driver.implicitly_wait(50)
 
-    # @unittest.SkipTest
-    def test_by_twtter(self):
-        url = 'https://twitter.com/login'
-        id = 'shivashankarawati@gmail.com'
-        password = 'Pgpgpgagj100.'
-        driver.get(url)
-        user = WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.NAME, "session[username_or_email]")))
-        user.send_keys(id)
-        # user.send_keys(Keys.ENTER)
-        time.sleep(3)
-        pw = driver.find_element(By.NAME, "session[password]")
-        pw.send_keys(password)
-        # pw.send_keys(Keys.ENTER)
-        driver.find_element_by_xpath("//*[@id='react-root']/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div/span/span").click()
 
-    # @classmethod
-    # def tearDown(self) -> None:
-    #     self.driver.close()
+    def test_fb(self):
+        
 
+        for r in range(2, rows + 1):
+            try:
+                page_title = xl.read_data(xl_sheet_path, 'sheet1', row=r, col=3)
+                login_id = xl.read_data(xl_sheet_path, 'sheet1', row=r, col=4)
+                password = xl.read_data(xl_sheet_path, 'sheet1', row=r, col=5)
+                self.assertEqual(page_title, driver.title, "page title is not matching")
+                user = WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.NAME, "email")))
+                user.send_keys(login_id).submit()
+                pw = driver.find_element(By.NAME, "pass")
+                pw.send_keys(password).submit()
+                driver.save_screenshot("/Users/shiv/PycharmProjects/automation/screenshots/row{}.png".format(r))
+                # xl.write_data(xl_sheet_path, sheet1, row=r, col=5, test_result)
+                # xl.write_data(xl_sheet_path, sheet1, row=r, col=6, Error_message)
+                # xl.write_data(xl_sheet_path, sheet1, row=r, col=7, screen_shot)
+
+            except:
+                driver.save_screenshot("/Users/shiv/PycharmProjects/automation/screenshots/Error{}.png".format(r))
 
     @classmethod
     def tearDownClass(cls) -> None:
         time.sleep(2)
-        cls.driver.quit()
+        driver.quit()
 
 
 if __name__=="__main__":
